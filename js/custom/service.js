92ddd42aabef909ef2a73a32ea34301294caad5e
git_vipul <vipul.jain0@gmail.com>
2014-12-09 17:10:44 +0530

    Bug Solved on intro Resource

--- js/custom/service.js
+++ js/custom/service.js
@@ -154,7 +154,7 @@ function fnOverlayContentOnBergeronSlide(data){
 };
 
 function fnOverlayImageContentOnGatherResourcesSlide(data){
-    $('#slide-dyn').attr('src', 'Images/67mHodTVPPD_DX1890_DY1890_CX945_CY530.png').fadeTo(2000);
+    $('#slide-dyn').attr('src', 'Images/68mHodTVPPD_DX1890_DY1890_CX945_CY530.png').fadeTo(2000);
     $('#nested-slide4-1').fadeTo(1000, 0, function () {
         $('#nested-slide4-1').attr('src',"Images/5VEQskudflI_80_DX1088_DY1088_CX544_CY304.png");
         $('#nested-slide4-1').removeClass('content-collapse');
